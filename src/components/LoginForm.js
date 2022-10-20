6bfec681000f43bad1902a7a443fcb2f50d3d903
Rizal Mahadi A B <rizal.mahadi.a.b@merck.com>
2022-10-20 09:32:36 +0800

    Initial setup

---
+++ src/components/LoginForm.js
@@ -0,0 +1,14 @@
+const LoginForm = () => {
+    return <>
+        <form>
+            <label>Username</label>
+            <input></input><br/>
+            <label>Password</label>
+            <input></input><br/>
+            <input type="submit"/>
+            <p>Login with google</p>
+        </form>
+    </>
+}
+
+export default LoginForm
